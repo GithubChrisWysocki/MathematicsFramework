6d0672ac58dd08d5fd6a29527cb7638ee3680e86
chris <christof_wysocki@outlook.de>
2024-12-20 03:06:57 +0100

    test3

--- MathematicsFramework/SetOperations.cs
+++ MathematicsFramework/SetOperations.cs
@@ -54,14 +54,14 @@ namespace MathematicsFramework
                 mathSet.GetAllMember().Add(item);
             }
         }
-        public static bool IsSetMember<T>(this Set<T> set, Set<T> memberOfSet) where T : ISetMember
-        {
-            T member = (T)set;
-            foreach (var item in set.GetAllMember())
-            {
-                memberOfSet.GetAllMember().Contains(set);
-            }
-            return true;
-        }
+        //public static bool IsSetMember<T>(this Set<T> set, Set<T> memberOfSet) where T : ISetMember
+        //{
+        //    T member = (T)set;
+        //    foreach (var item in set.GetAllMember())
+        //    {
+        //        memberOfSet.GetAllMember().Contains(set);
+        //    }
+        //    return true;
+        //}
     }
 }
